ee8c025ec19179aa844cf62669dd8d33de58cde0
Danny Ramos <danramos21@gmail.com>
2020-04-14 11:40:28 -0600

    Practice makes perfect

--- lib/activity.rb
+++ lib/activity.rb
@@ -8,26 +8,11 @@ class Activity
     @participants = {}
   end
 
-  def add_participant(participant, cost)
-    @participants[participant] = cost
+  def add_participant(name, total_cost)
+    @participants[name] = total_cost
   end
 
   def total_cost
-    @participants.values.map(&@participant).reduce(:+)
-  end
-
-  def split
-    total_cost / @participants.count
-  end
-
-  # For the `owed` method, the amount each person owes is the
-  # difference between what they paid and the `split`.
-
-  def owed
-    debt = {}
-    @participants.each do |participant, cost|
-      debt[participant] = (split - cost)
-    end
-    debt
+    @participants.values.sum
   end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -2,7 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/activity'
 
-class AvtivityTest < Minitest::Test
+class ActivityTest < Minitest::Test
   def setup
     @activity = Activity.new("Brunch")
     end
@@ -16,31 +16,14 @@ class AvtivityTest < Minitest::Test
       assert_equal ({}), @activity.participants
     end
 
-    def test_it_has_participants
+    def test_it_can_add_participants
       @activity.add_participant("Maria", 20)
-      assert_equal ({"Maria" => 20}), @activity.participants
+      expected = {"Maria" => 20}
+      assert_equal expected, @activity.participants
     end
 
-    def test_it_has_total_cost
+    def test_it_has_a_total_cost
       @activity.add_participant("Maria", 20)
       assert_equal 20, @activity.total_cost
-      @activity.add_participant("Luther", 40)
-      assert_equal ({"Maria" => 20, "Luther" => 40}), @activity.participants
-      assert_equal 60, @activity.total_cost
     end
-
-    def test_it_can_split_cost
-      @activity.add_participant("Maria", 20)
-      @activity.add_participant("Luther", 40)
-      @activity.total_cost
-      assert_equal 30, @activity.split
-    end
-
-    def test_participant_is_owed
-      @activity.add_participant("Maria", 20)
-      @activity.add_participant("Luther", 40)
-      @activity.total_cost
-      expected = {"Maria" => 10, "Luther" => -10}
-      assert_equal expected, @activity.owed
-    end
-end
+  end

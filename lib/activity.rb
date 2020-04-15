32f26d542d0947780e761220053890f20238b0e7
Danny Ramos <danramos21@gmail.com>
2020-04-14 18:43:16 -0600

    Practice practice

--- lib/activity.rb
+++ lib/activity.rb
@@ -15,4 +15,22 @@ class Activity
   def total_cost
     @participants.values.sum
   end
+
+# It should be the total
+# cost divided by the number of participants.
+
+  def split
+    total_cost / @participants.count
+  end
+
+# the amount each person owes is the difference
+# between what they paid and the `split`
+
+  def owed
+    debt = {}
+    @participants.each do |participant|
+      debt[participant.first] = split - participant.last
+    end
+    debt
+  end
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -25,5 +25,24 @@ class ActivityTest < Minitest::Test
     def test_it_has_a_total_cost
       @activity.add_participant("Maria", 20)
       assert_equal 20, @activity.total_cost
+      @activity.add_participant("Luther", 40)
+      expected = {"Maria" => 20, "Luther" => 40}
+      assert_equal expected, @activity.participants
+      assert_equal 60, @activity.total_cost
+    end
+
+    def test_it_can_split
+      @activity.add_participant("Maria", 20)
+      @activity.add_participant("Luther", 40)
+      @activity.total_cost
+      assert_equal 30, @activity.split
+    end
+
+    def test_amount_participants_owed
+      @activity.add_participant("Maria", 20)
+      @activity.add_participant("Luther", 40)
+      @activity.total_cost
+      expected = {"Maria" => 10, "Luther" => -10}
+      assert_equal expected, @activity.owed
     end
   end
